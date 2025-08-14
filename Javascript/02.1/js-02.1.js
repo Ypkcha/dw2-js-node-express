e0c3f2ffd21325d42808c15f5d1d02e51ce9ba63
Yuri <fatec-dsm2@academico.intra>
2025-08-14 19:25:11 -0300

    Commit com o começo da aula 2 sobre arrays e objetos

---
+++ Javascript/02.1/js-02.1.js
@@ -0,0 +1,16 @@
+const products = ["Computador", "Notebook", "Celular", "Tablet"];
+document.write(`<p>${products}</p>`);
+document.write(typeof products);
+document.write("<p>Exibindo um elemento do array através do índice:</p>");
+document.write(`<p>${products[1]}</p>`);
+
+document.write("<p>Percorrendo um array através do forEach</p>");
+products.forEach((products) => {
+  document.write(`<p>${products}</p>`);
+});
+//Traduzindo acima: Para cada elemento dentro da array escreva no documento...
+
+document.write("<p>Mostrando os indices e valores com forEach</p>");
+products.forEach((products, i) => {
+  document.write(`<p>${i + 1} - ${products}</p>`);
+});
