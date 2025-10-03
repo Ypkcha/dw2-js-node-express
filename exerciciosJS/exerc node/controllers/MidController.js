442b2d9c2ef2ba5ca3ec8426947df77ad4b96f8a
Yuri <fatec-dsm2@academico.intra>
2025-10-03 19:41:31 -0300

    commit com o exercicio feito só falta colocar uns frufru com css

--- exerciciosJS/exerc node/controllers/MidController.js
+++ exerciciosJS/exerc node/controllers/MidController.js
@@ -4,11 +4,11 @@ const router = express.Router();
 // conteudo da rota
 router.get("/mid", (req, res) => {
   const midLaners = [
-    { campeao: "Orianna", classe: "Mago", motivo: "---" },
-    { campeao: "Aurora", classe: "Mago", motivo: "---" },
-    { campeao: "Anivia", classe: "Mago", motivo: "---" },
-    { campeao: "Lissandra", classe: "Mago", motivo: "---" },
-    { campeao: "Veigar", classe: "Mago", motivo: "---" },
+    { campeao: "Orianna", classe: "Mago", motivo: "Eu jogo com ela por conta dela ser bem consistente e ter uma mecânica bem legal com o orbe dela" },
+    { campeao: "Aurora", classe: "Mago", motivo: "Tem vezes que ela simplesmente impossibilita quem você tá contra de jogar e eu acho isso engraçado" },
+    { campeao: "Anivia", classe: "Mago", motivo: "Não sei dizer o exato motivo de gostar de jogar com ela, ela parece ser tão simples e entediante mas algo me encanta nisso" },
+    { campeao: "Neeko", classe: "Mago", motivo: "Ela é um dos campeões que eu acho mais divertido de jogar, ainda mais por não precisar ficar forte pra ela ser útil" },
+    { campeao: "Veigar", classe: "Mago", motivo: "Foi um dos primeiros que eu joguei então eu tenho um certo apego por ele" },
   ];
   res.render("mid", {
     midLaners: midLaners,
