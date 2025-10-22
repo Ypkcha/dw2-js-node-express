1915ae15903dbbd1b3efd0b8b7b042f3128cefcd
Yuri <fatec-dsm2@academico.intra>
2025-10-21 21:24:13 -0300

    commit atrasado da aula 5 de CRUD

--- Node.js/nodejs-aula-05-SistemaLoja/nodejs-aula-05-SistemaLoja/controllers/ClientesController.js
+++ Node.js/nodejs-aula-05-SistemaLoja/nodejs-aula-05-SistemaLoja/controllers/ClientesController.js
@@ -6,10 +6,83 @@ import Cliente from "../models/Cliente.js";
 // ROTA CLIENTES
 router.get("/clientes", function (req, res) {
   // SELECT * FROM CLIENTES
-  Cliente.findAll().then((clientes) => {
-    res.render("clientes", {
-      clientes: clientes,
+  Cliente.findAll()
+    // then ==> promessa bem sucedida
+    .then((clientes) => {
+      res.render("clientes", {
+        clientes: clientes,
+      });
+    })
+    // catch ==> falha na resolução da promessa
+    .catch((error) => {
+      console.log(error);
     });
+});
+
+// Rota de cadastro de clientes
+router.post("/clientes/new", (req, res) => {
+  // Colentando os dados do formulario
+  const nome = req.body.nome; //(o ultimo "nome" é o "name" do formulário)
+  const cpf = req.body.cpf;
+  const endereco = req.body.endereco;
+  Cliente.create({
+    // coluna : dado do formulário
+    nome: nome,
+    cpf: cpf,
+    endereco: endereco,
+  })
+    .then(() => {
+      res.redirect("/clientes");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+// Rota de exclusão de cliente
+// :id é um parâmetro obrigatório
+router.get("/clientes/delete/:id", (req, res) => {
+  const id = req.params.id; // pegando o id através do parâmetro (params)
+  // .destroy() -> excluir um registro do banco
+  Cliente.destroy({
+    where: {
+      id: id,
+    },
+  })
+    .then(() => {
+      res.redirect("/clientes");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+// Rota de edição de clientes
+router.get("/clientes/edit/:id", (req, res) => {
+  const id = req.params.id;
+  // Buscando o cliente pela ID
+  // findByPk() -> busca um registro pela chave primária (id)
+  Cliente.findByPk(id).then((cliente) => {
+    res.render("clienteEdit", {
+      cliente: cliente,
+    });
+  });
+});
+
+router.post("/clientes/update/:id", (req, res) => {
+  const id = req.body.id;
+  const nome = req.body.nome;
+  const cpf = req.body.cpf;
+  const endereco = req.body.endereco;
+  Cliente.update(
+    {
+      nome: nome,
+      cpf: cpf,
+      endereco: endereco,
+    },
+    { where: { id: id } }
+  ).then(() => {
+    res.redirect("/clientes");
   });
 });
 
--- Node.js/nodejs-aula-05-SistemaLoja/nodejs-aula-05-SistemaLoja/controllers/PedidosController.js
+++ Node.js/nodejs-aula-05-SistemaLoja/nodejs-aula-05-SistemaLoja/controllers/PedidosController.js
@@ -2,13 +2,71 @@ import express from "express";
 const router = express.Router();
 
 import Pedido from "../models/Pedido.js";
-
 // ROTA PEDIDOS
 router.get("/pedidos", function (req, res) {
   Pedido.findAll().then((pedidos) => {
     res.render("pedidos", {
       pedidos: pedidos,
     });
+  });
+});
+
+//Rota de cadastro de pedidos
+router.post("/pedidos/new", (req, res) => {
+  // Colentando os dados do formulario
+  const numero = req.body.numero; //(o ultimo "nome" é o "name" do formulário)
+  const valor = req.body.valor;
+  Pedido.create({
+    // coluna : dado do formulário
+    numero: numero,
+    valor: valor,
+  })
+    .then(() => {
+      res.redirect("/pedidos");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+//rota de exclusão de pedidos
+router.get("/pedidos/delete/:id", (req, res) => {
+  const id = req.params.id;
+  Pedido.destroy({
+    where: {
+      id: id,
+    },
+  })
+    .then(() => {
+      res.redirect("/pedidos");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+//Rota de edição
+router.get("/pedidos/update/:id", (req, res) => {
+  const id = req.params.id;
+  Pedido.findByPk(id).then((pedido) => {
+    res
+      .render("/pedidoEdit", {
+        pedido: pedido,
+      });
+      });
+  });
+router.post("/pedidos/update/:id", (req, res) => {
+  const id = req.body.id;
+  const numero = req.body.numero;
+  const valor = req.body.valor;
+  Pedido.update(
+    {
+      numero: numero,
+      valor: valor,
+    },
+    { where: { id: id } }
+  ).then(() => {
+    res.redirect("/pedidos");
   });
 });
 
